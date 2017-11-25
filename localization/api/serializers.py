5315d60bc039ea973a1602a55cf60ef4308d1ca3
deepanshut041 <deepanshut041@gmail.com>
2017-11-25 16:53:44 +0530

    Now working on car model

--- localization/api/serializers.py
+++ localization/api/serializers.py
@@ -6,9 +6,9 @@ class DriverModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Driver
         fields = [
-            'driver_id',
+            'driver_dl',
             'driver_name',
             'driver_dob',
             'driver_uidai',
-            'driver_password'
+            'user',
         ]
--- localization/models.py
+++ localization/models.py
@@ -8,8 +8,10 @@ class Driver(models.Model):
     driver_name = models.CharField(max_length=150)
     driver_dob = models.DateField()
     driver_uidai = models.IntegerField(unique=True)
-    driver_password = models.CharField(max_length=30,default="123456")
     driver_dl = models.IntegerField(unique=True)
 
     def __str__(self):
         return str(self.driver_name)
+class Car(models.Model):
+    car_id = models.CharField(max_length=20)
+    car_password = models.CharField(max_length=150)
