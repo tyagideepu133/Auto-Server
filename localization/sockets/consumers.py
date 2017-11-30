61f45ee34ae28bc02f29314b930e9050b7c8f03f
deepanshut041 <deepanshut041@gmail.com>
2017-12-01 01:26:03 +0530

    wrote car update algorithm

--- localization/sockets/consumers.py
+++ localization/sockets/consumers.py
@@ -2,6 +2,7 @@ from channels.generic.websockets import WebsocketDemultiplexer, JsonWebsocketCon
 from ..models import CarStatus
 from ..api.serializers import CarStatusModelSerializer
 from django.http import Http404
+from math import sin, cos, sqrt, atan2, radians
 
 
 class StatusConsumer(JsonWebsocketConsumer):
@@ -12,6 +13,14 @@ class StatusConsumer(JsonWebsocketConsumer):
         except CarStatus.DoesNotExist:
             raise Http404
 
+    def get_cars_radius(self, car_lat, car_lon):
+        sm_car_lat = float(car_lat) - 0.020000
+        lg_car_lat = float(car_lat) + 0.020000
+        sm_car_lon = float(car_lon) - 0.030000
+        lg_car_lon = float(car_lon) + 0.030000
+        cars = CarStatus.objects.filter(car_lat__lte=lg_car_lat, car_lat__gte=sm_car_lat,car_lon__lte=lg_car_lon, car_lon__gte=sm_car_lon)
+        return cars
+
     def connect(self, message, multiplexer, **kwargs):
         multiplexer.send({"status": "I just connected!"})
 
@@ -21,6 +30,9 @@ class StatusConsumer(JsonWebsocketConsumer):
         serializer = CarStatusModelSerializer(car_status, data=content, partial=True)
         if serializer.is_valid():
             serializer.save()
+        cars = self.get_cars_radius(content['car_lat'], content['car_lon'])
+        cars_serializer = CarStatusModelSerializer(cars, many=True)
+        multiplexer.send(cars_serializer.data)
 
 
     def disconnect(self, message, multiplexer, **kwargs):
